051f9d3f035ccd72f11c650d1cf25e7c77c79c1f
Vadim Katkov <vmalyh777@gmail.com>
2024-05-02 22:29:05 +0300

    Проверка работоспособности функциональности менеджера

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,64 @@
-import Manager.Manager;
+import Manager.*;
 import Tasks.*;
 
 import java.util.Map;
 import java.util.HashMap;
 
 public class Main {
-
     public static void main(String[] args) {
-        Map<Integer, Object> tasksList = Manager.tasksList;
+        Manager manager = new Manager();
 
+        Task task1 = new Task("Тренировка", "Вот так вот");
+        Task task2 = new Task("Вымыть кота", "Найти и запихать в ванну");
+        Task task3 = new Task("Найти планету сокровищ", "Нам нужен парень с большими задатками");
+        Epic epic1 = new Epic("Попасть в исекай", "Почему бы и нет");
+        Subtask epic1Subtask1 = new Subtask("Найти белый грузовичок", "Самый надежный способ");
+        Epic epic2 = new Epic("Сходить погулять", "Нет, дома ты не погуляешь");
+        Subtask epic2Subtask1 = new Subtask("Найти мотивацию", "Чем больше, тем лучше");
+        Subtask epic2Subtask2 = new Subtask("Выйти погулять", "Это ножками делается");
 
-       System.out.println(tasksList);
+        manager.addTask(task1);
+        manager.addTask(task1);
+        manager.addTask(task2);
+        manager.addTask(epic1);
+        manager.addTask(epic1, epic1Subtask1);
+        manager.addTask(epic2);
+        manager.addTask(epic2, epic2Subtask1);
+        manager.addTask(epic2, epic2Subtask1, epic2Subtask2);
 
+        System.out.println(manager);
+
+        System.out.println("\nУдаление задач");
+        manager.deleteTask(1,2,4);
+
+        System.out.println("Обновление статусов");
+        manager.updateStatus(task1);
+        manager.updateStatus(epic2Subtask1);
+        manager.updateStatus(epic2);
+
+        System.out.println(manager);
+
+        System.out.println("\nДобавление удаленных и новых задач ");
+        manager.addTask(epic1);
+        manager.addTask(task2);
+        manager.addTask(task3);
+
+        System.out.println(manager);
+
+        System.out.println("Получение списка задач\n" + manager.getTasksList());
+
+        System.out.println("Получение определенной задачи\n" + manager.getTask(2));
+
+        System.out.println("Получение подзадач эпика\n" + manager.getSubtasks(epic2));
+
+        System.out.println("Обновление задачи\n");
+        manager.updateTask(1, manager.updateInformation(manager.getTask(1), "description",
+                "Он прячется в шкафу"));
+
+        System.out.println(manager);
+
+        manager.deleteAllTasks();
+
+        System.out.println(manager);
     }
 }
