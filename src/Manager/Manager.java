0ac3a201cc47b6854f818ad90faacf1e791d2908
Vadim Katkov <vmalyh777@gmail.com>
2024-05-02 17:56:28 +0300

    Реализована логика работы класса Manager

---
+++ src/Manager/Manager.java
@@ -0,0 +1,60 @@
+package Manager;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+
+import Tasks.*;
+
+public class Manager {
+    static protected int identifier = 0;
+    public Map<Integer, Object> tasksMap = new HashMap<>();
+
+    public void addTask(Object o) {
+        identifier++;
+        tasksMap.put(identifier, o);
+    }
+
+    public void addTask(Epic epic, Subtask subtask) {
+        epic.addSubtask(subtask);
+    }
+
+    public List getTasksList() {
+        List<Object> tasksList = new ArrayList<>(tasksMap.values());
+        return tasksList;
+    }
+
+    public void deleteAllTask(){
+        for (int i : tasksMap.keySet()) {
+            tasksMap.remove(i);
+        }
+    }
+
+    public void deleteTask(int index){
+        tasksMap.remove(index);
+    }
+
+    public Object getTask(int index){
+        return tasksMap.get(index);
+    }
+
+    public List<Subtask> getSubtasks(Epic epic){
+        List<Subtask> list = new ArrayList<>(epic.getSubtasksList());
+        return list;
+    }
+
+    public void update(int index, Object o){
+        deleteTask(index);
+        tasksMap.put(index, o);
+    }
+
+    @Override
+    public String toString(){
+        String result = "";
+        for(int i : tasksMap.keySet()){
+            result += "\t\t" + i + "\t\t\n" + tasksMap.get(i);
+        }
+        return result;
+    }
+}
