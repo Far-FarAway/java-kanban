334c937449dc2cbab6e9c83788d5d101d1a300b9
Vadim Katkov <vmalyh777@gmail.com>
2024-05-12 00:30:44 +0300

    Удален геттер id

--- src/Manager/TaskManager.java
+++ src/Manager/TaskManager.java
@@ -22,8 +22,6 @@ public interface TaskManager {
 
     List<Subtask> getSubtasks(Epic epic);
 
-    int getId();
-
     void updateTask(int index, Task task);
 
     Task updateInformation(Task task, String nameOrDescription, String info);
