7733ed3bc24049f1a6763800d8fffd58d91fda24
Vadim Katkov <vmalyh777@gmail.com>
2024-05-11 11:33:38 +0300

    Оздан интерфейс TaskManager

---
+++ src/Manager/TaskManager.java
@@ -0,0 +1,30 @@
+package Manager;
+
+import Task.*;
+
+import java.util.List;
+
+public interface TaskManager {
+    void addTask(Task o);
+
+    void addTask(Epic epic, Subtask... subtasks);
+
+    List<Task> getTasksList();
+
+    void deleteAllTasks();
+
+    void deleteByIds(int... indexes);
+
+    Task getTask(int index);
+
+    List<Subtask> getSubtasks(Epic epic);
+
+    void updateTask(int index, Task task);
+
+    Task updateInformation(Task task, String nameOrDescription, String info);
+
+    @Override
+    String toString();
+
+    void updateStatus(Task task);
+}
