e3091e6a0583da3d72f6e9e84c9521f5586d9bd7
Vadim Katkov <vmalyh777@gmail.com>
2024-05-14 15:12:11 +0300

    Создан и реализован клас InMemoryHistoryManager

---
+++ src/Manager/InMemoryHistoryManager.java
@@ -0,0 +1,45 @@
+package Manager;
+
+import Task.Task;
+
+import java.util.ArrayList;
+
+public class InMemoryHistoryManager implements HistoryManager{
+
+    protected ArrayList<Task> history = new ArrayList<>();
+
+    @Override
+    public void add(Task task){
+        history.add(task);
+        checkHistory();
+    }
+
+    @Override
+    public String toString(){
+        String result = "История просмотров:\n";
+        if(!history.isEmpty()) {
+            for (Task task : history) {
+                result += task;
+            }
+        } else {
+            result += "Пока пуста";
+        }
+
+        return result;
+    }
+    @Override
+    public ArrayList<Task> getHistory(){
+        return history;
+    }
+
+    private void checkHistory(){
+        if (history.size() > 10){
+            for(Task task : history){
+                history.remove(task);
+                if(history.size() < 10){
+                    break;
+                }
+            }
+        }
+    }
+}
