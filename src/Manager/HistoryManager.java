6303e2da4694c40ba338a6fd43ec554f67f8bb27
Vadim Katkov <vmalyh777@gmail.com>
2024-05-15 21:04:45 +0300

    Тип возвращаемого значения метода getHistory изменен на List

--- src/Manager/HistoryManager.java
+++ src/Manager/HistoryManager.java
@@ -1,10 +1,10 @@
 package Manager;
 
 import Task.*;
-import java.util.ArrayList;
+import java.util.List;
 
 public interface HistoryManager {
     void add(Task task);
 
-    ArrayList<Task> getHistory();
+    List<Task> getHistory();
 }
