03c7db379c3719d9f71a24b9fc1b4adc2e08c1e8
Vadim Katkov <vmalyh777@gmail.com>
2024-07-05 21:56:34 +0300

    Реализовать метод для загрузки списка задач при начале работы программы

--- src/manager/FileBackedTaskManager.java
+++ src/manager/FileBackedTaskManager.java
@@ -1,7 +1,10 @@
 package manager;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 
 import task.*;
@@ -34,45 +37,62 @@ public class FileBackedTaskManager extends InMemoryTaskManager implements TaskMa
         }
     }
 
+    public static void loadFromFile(File file) {
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            while (br.ready()){
+                String[] info = br.readLine().split(",");
+                if (info[1].equals("TASK")){
+                    tasksMap.put(Integer.parseInt(info[0]), new Task(info[2], info[4], Status.valueOf(info[3])));
+                } else if (info[1].equals("EPIC")) {
+                    tasksMap.put(Integer.parseInt(info[0]), new Epic(info[2], info[4], Status.valueOf(info[3])));
+                } else {
+                    tasksMap.put(Integer.parseInt(info[0]), new Subtask(info[2], info[4], Status.valueOf(info[3])));
+                }
+            }
+        } catch (IOException ex) {
+            System.out.println("Исключение во время попытки загрузить данные задач из файла");
+        }
+    }
+
     @Override
-    public void addTask(Task o){
+    public void addTask(Task o) {
         super.addTask(o);
         save();
     }
 
     @Override
-    public void addTask(Epic epic, Subtask... subtasks){
+    public void addTask(Epic epic, Subtask... subtasks) {
         super.addTask(epic, subtasks);
         save();
     }
 
     @Override
-    public void deleteAllTasks(){
+    public void deleteAllTasks() {
         super.deleteAllTasks();
         save();
     }
 
     @Override
-    public void deleteByIds(int... indexes){
+    public void deleteByIds(int... indexes) {
         super.deleteByIds(indexes);
         save();
     }
 
     @Override
-    public void updateTask(int index, Task task){
+    public void updateTask(int index, Task task) {
         super.updateTask(index, task);
         save();
     }
 
     @Override
-    public Task updateInformation(Task task, String nameOrDescription, String info){
+    public Task updateInformation(Task task, String nameOrDescription, String info) {
         Task updatedTask = super.updateInformation(task, nameOrDescription, info);
         save();
         return updatedTask;
     }
 
     @Override
-    public void updateStatus(Task task){
+    public void updateStatus(Task task) {
         super.updateStatus(task);
         save();
     }
