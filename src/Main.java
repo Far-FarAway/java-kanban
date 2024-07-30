2b3d4f31bea22dfedfe702a971c7ef2d8d3d42e2
Vadim Katkov <vmalyh777@gmail.com>
2024-07-30 12:48:10 +0300

    Первичная проверка работоспособности

--- src/Main.java
+++ src/Main.java
@@ -9,23 +9,49 @@ public class Main {
         Task task2 = new Task("Вымыть кота", "Найти и запихать в ванну", 240,
                 "14.10.2024 08:00");
         Task task3 = new Task("Найти планету сокровищ", "Нам нужен парень с великими задатками",
-                30, "15.10.2024 15:00");
+                30, "11.10.2024 15:00");
         Epic epic1 = new Epic("Попасть в исекай", "Почему бы и нет");
-        Subtask epic1Subtask1 = new Subtask("Найти белый грузовичок", "Самый надежный способ", 10, "31.12.2024 23:50");
+        Subtask epic1Subtask1 = new Subtask("Найти белый грузовичок", "Самый надежный способ",
+                10, "31.12.2024 23:50");
         Epic epic2 = new Epic("Сходить погулять", "Нет, дома ты не погуляешь");
-        Subtask epic2Subtask1 = new Subtask("Найти мотивацию", "Чем больше, тем лучше", 360, "05.01.2025 12:00");
-        Subtask epic2Subtask2 = new Subtask("Выйти погулять", "Это ножками делается", 3, "05.01.2025 18:00");
+        Subtask epic2Subtask1 = new Subtask("Найти мотивацию", "Чем больше, тем лучше",
+                360, "05.01.2025 12:00");
+        Subtask epic2Subtask2 = new Subtask("Выйти погулять", "Это ножками делается",
+                3, "05.01.2025 18:00");
 
         manager.addTask(task1);
         manager.addTask(task1);
         manager.addTask(task2);
+        manager.addTask(task3);
         manager.addTask(epic1);
         manager.addTask(epic1, epic1Subtask1);
         manager.addTask(epic2);
+        manager.addTask(epic2, epic2Subtask2);
         manager.addTask(epic2, epic2Subtask1);
-        manager.addTask(epic2, epic2Subtask1, epic2Subtask2);
 
-        System.out.println(manager);
+        //System.out.println(manager);
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 50; j++) {
+                if(i%2==0) {
+                    if (j % 2 == 0) {
+                        System.out.print("øøø");
+                    } else {
+                        System.out.print(" ");
+                    }
+                } else {
+                    if (j % 2 == 0) {
+                        System.out.print("\t");
+                    } else {
+                        System.out.print("ø");
+                    }
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n");
+        manager.getPrioritizedTasks();
         /*
         System.out.println("\nУдаление задач");
         manager.deleteByIds(2,3);
